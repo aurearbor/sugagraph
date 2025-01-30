3931565a2e7e1c465a7617e177277246115530da
aurearbor <arthur.goldenbaum@gmail.com>
2025-01-30 23:55:24 +0100

    request seems to be accepted

--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 
 func main() {
 
-    resp, err := http.Get("https://api.example.com/data")
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer resp.Body.Close()
+    req, err := http.NewRequest("GET", "https://sandbox-api.dexcom.com/v3/users/self/egvs", nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer ZEmdyL0fGCYsC6vC")
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
 
     body, _ := io.ReadAll(resp.Body)
     fmt.Println(string(body))
-	
+
 }
